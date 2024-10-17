56fb6c0a8eb87a3d091c7ff4f3684a3283b38ce3
Brad Waropay <brad.waropay@gmail.com>
2024-10-17 14:45:43 -0500

    Package update (#14)

--- dist/types/tag.types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TagType;
-(function (TagType) {
-    TagType["GENERIC"] = "generic";
-    TagType["RAG"] = "rag";
-})(TagType || (TagType = {}));
-//# sourceMappingURL=tag.types.js.map
--- dist/types/transaction.types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TransactionLoggingMethod = void 0;
-var TransactionLoggingMethod;
-(function (TransactionLoggingMethod) {
-    TransactionLoggingMethod["js_langchain"] = "js_langchain";
-    TransactionLoggingMethod["js_logger"] = "js_logger";
-})(TransactionLoggingMethod || (exports.TransactionLoggingMethod = TransactionLoggingMethod = {}));
-//# sourceMappingURL=transaction.types.js.map
