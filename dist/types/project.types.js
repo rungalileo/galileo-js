98f25c76a4195e45d7723aa42a06b34e2a6e5245
Brad Waropay <brad.waropay@gmail.com>
2024-10-17 14:43:06 -0500

    Update package.json

--- dist/types/project.types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProjectTypes = void 0;
-var ProjectTypes;
-(function (ProjectTypes) {
-    ProjectTypes["evaluate"] = "prompt_evaluation";
-    ProjectTypes["observe"] = "llm_monitor";
-})(ProjectTypes || (exports.ProjectTypes = ProjectTypes = {}));
-//# sourceMappingURL=project.types.js.map
