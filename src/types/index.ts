7aa70afa34a065dc40a4c6b4e1d1c06c8f28d3ff
ajaynayak <ajaynayak@gmail.com>
2025-04-08 13:57:45 -0700

    feat: making it easier to import types using "galileo/types"

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+// Re-export types from selected individual type files
+export * from './message.types';
+export * from './document.types';
+export * from './log.types';
+export * from './prompt-template.types';
+export * from './dataset.types';
+export * from './experiment.types';
+export * from './project.types';
+export * from './log-stream.types';
+export * from './models.types';
+export * from './scorer.types';
