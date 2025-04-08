200998b5dd917507c4211de2b7c0bd0132058260
ajaynayak <ajaynayak@gmail.com>
2025-04-08 09:47:48 -0700

    test fix

--- tests/utils/serialization.test.ts
+++ tests/utils/serialization.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { extractParamsInfo, argsToDict } from '../../src/utils/serialization';
 
 describe('Extract Function Parameters', () => {
