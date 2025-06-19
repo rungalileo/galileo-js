3d1eb0327f71dfdea6456615171daaf989167171
Juan Andres Ramil <juan@rungalileo.io>
2025-06-19 14:49:03 -0300

    feat: Add Global Prompt Template functionality and service integration

--- src/types/prompt-template.types.ts
+++ src/types/prompt-template.types.ts
@@ -4,3 +4,5 @@ export type PromptTemplate =
   components['schemas']['BasePromptTemplateResponse'];
 export type PromptTemplateVersion =
   components['schemas']['BasePromptTemplateVersionResponse'];
+export type ListPromptTemplateResponse =
+  components['schemas']['ListPromptTemplateResponse'];
