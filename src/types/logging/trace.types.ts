c11ee5b217187283f451305e79098f14c3cb40ea
Juan D. <juandrucker@rungalileo.io>
2025-07-25 14:31:56 -0300

    feat: support ingesting redacted input and redacted output (#262)

--- src/types/logging/trace.types.ts
+++ src/types/logging/trace.types.ts
@@ -5,17 +5,23 @@ import { StepType } from './step.types';
 
 export interface TraceOptions extends StepWithChildSpansOptions {
   input: string;
+  redactedInput?: string;
   output?: string;
+  redactedOutput?: string;
 }
 
 export class Trace extends StepWithChildSpans {
   type: StepType = StepType.trace;
   input: string;
+  redactedInput?: string;
   output?: string;
+  redactedOutput?: string;
 
   constructor(data: TraceOptions) {
     super(StepType.trace, data);
     this.input = data.input;
+    this.redactedInput = data.redactedInput;
     this.output = data.output;
+    this.redactedOutput = data.redactedOutput;
   }
 }
