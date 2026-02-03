43583c03eb62173d84ab370309961b1f6bb48dee
Vanessa Soares <vanessafsoares@hotmail.com>
2026-02-03 17:22:54 -0300

    chore(coverage): Add Codecov configuration and update Jest settings for coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'json', 'html'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/types/**/*',
+    '!src/**/*.types.ts'
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/tests/'
+  ]
 };
