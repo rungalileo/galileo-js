065460b536a0515a867c97ee2236d88ea9b05342
galileo-automation <ci@rungalileo.io>
2026-08-18 13:17:02 +0000

    chore(types): Update API Types

--- src/entities/experiments.ts
+++ src/entities/experiments.ts
@@ -634,7 +634,8 @@ export class Experiments {
 
   private async configureExperimentMetrics(
     metrics:
-      (GalileoMetrics | string | Metric | LocalMetricConfig)[] | undefined,
+      | (GalileoMetrics | string | Metric | LocalMetricConfig)[]
+      | undefined,
     projectId: string,
     experimentId: string
   ): Promise<[ScorerConfig[], LocalMetricConfig[]]> {
@@ -742,7 +743,8 @@ export class Experiments {
     projectName: string,
     projectId: string,
     metrics:
-      (GalileoMetrics | string | Metric | LocalMetricConfig)[] | undefined,
+      | (GalileoMetrics | string | Metric | LocalMetricConfig)[]
+      | undefined,
     promptTemplate: PromptTemplateType,
     promptSettings: PromptRunSettings
   ): Promise<RunExperimentOutput> {
